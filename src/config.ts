9dc6e9ea33ff594db5b091bfc1f9869564fda441
wzy <ziyue0530@gmail.com>
2025-04-29 12:42:59 +0800

    bug fix

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ import { PublicKey } from "@solana/web3.js";
 export const PROGRAM_ID = new PublicKey("FYpxHqGByHTMTPZ2gda59kvrVcA7Camx5LHFN151zr53"); // Your Program ID
 export const USDC_MINT_ADDRESS = new PublicKey("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"); // Verify Devnet USDC!
 export const CENTRAL_VAULT_TOKEN_ACCOUNT = new PublicKey("4dT8ogcUysTuxwk2UUYbZ9izaxqbnGzukdVvaus6QobN"); // Get from backend/config
-export const BACKEND_URL = "app"; // Your backend API URL
+export const BACKEND_URL = "api"; // Your backend API URL
 export const RPC_ENDPOINT = "https://devnet.helius-rpc.com/?api-key=b5c39727-26dc-4974-aaee-31388a50e9bc"
